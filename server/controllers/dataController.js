86ccb4a514f7e77fea9d5888aeb108bf6429bbae
Brittney VandenBos <bvandenbos@decwise.local>
2019-05-15 16:15:00 -0600

    active county

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -3,5 +3,12 @@ module.exports = {
         const db = req.app.get('db');
         const countyData = await db.get_county_data();
         res.status(200).send(countyData);
+    },
+    getActiveCounty: async (req, res) => {
+        const db = req.app.get('db');
+        const {county_id} = req.params;
+        let countyArr = await db.get_active_county(county_id);
+        let county = countyArr[0];
+        res.status(200).send(county);
     }
 };
