64f0d9cbdcc5a14697351bfdcb64059616a26e1c
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-05-20 10:42:54 -0600

    scaling working

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import ActiveCounty from './ActiveCounty';
-import { getCountyData, standardDeviation, getActiveCounty } from "./../ducks/countyReducer";
+import ActiveCounty from "./ActiveCounty";
+import {
+  getCountyData,
+  standardDeviation,
+  getActiveCounty
+} from "./../ducks/countyReducer";
 import { addFavorite } from "./../ducks/favoritesReducer";
 
 import * as d3 from "d3";
@@ -12,15 +16,15 @@ class Map extends Component {
     super(props);
 
     this.state = {
-      dataset: 'household_income_stdev',
+      dataset: "household_income_stdev",
       hi: true,
-        hi_val: 60000,
+      hi_val: 60000,
       pv: true,
-        pv_val: 217600,
+      pv_val: 217600,
       age: true,
-        age_val: 38,
+      age_val: 38,
       c: true,
-        c_val: 23  
+      c_val: 23
     };
   }
   async componentDidMount() {
@@ -39,11 +43,12 @@ class Map extends Component {
     const svg = d3
       .select(".map-container")
       .append("svg")
-      .attr("width", 960)
-      .attr("height", 600);
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("viewBox", "0 0 960 600")
 
-    const combinedData = topojson.feature(usGeoData, usGeoData.objects.counties).features;
-
+    const combinedData = topojson.feature(usGeoData, usGeoData.objects.counties)
+      .features;
 
     function combineData() {
       for (let i = 0; i < combinedData.length; i++) {
@@ -73,52 +78,113 @@ class Map extends Component {
       .attr("d", geoPath)
       .attr("id", d => d.id)
       .attr("fill", function shader(d) {
-        const { hi, hi_val, pv, pv_val, age, age_val, c, c_val } = mapContext.state
-        const {household_income_stdev, property_value_stdev, median_age_stdev, commute_time_stdev} = mapContext.props.county.standardDeviation;
+        const {
+          hi,
+          hi_val,
+          pv,
+          pv_val,
+          age,
+          age_val,
+          c,
+          c_val
+        } = mapContext.state;
+        const {
+          household_income_stdev,
+          property_value_stdev,
+          median_age_stdev,
+          commute_time_stdev
+        } = mapContext.props.county.standardDeviation;
 
-        const color8 = '#edfdff' 
-        const color7 = '#a6f7ff'
-        const color6 = '#71f3ff'
-        const color5 = '#3cefff'
-        const color4 = '#32c4d1'
-        const color3 = '#21838c'
-        const color2 = '#16575d'
-        const color1 = '#0b2c2f'
+        const color8 = "#edfdff";
+        const color7 = "#a6f7ff";
+        const color6 = "#71f3ff";
+        const color5 = "#3cefff";
+        const color4 = "#32c4d1";
+        const color3 = "#21838c";
+        const color2 = "#16575d";
+        const color1 = "#0b2c2f";
 
-        let datasetArr = [{datatype: 'household_income', input: Number(hi_val), sd: Number(household_income_stdev), include: hi}, {datatype: 'property_value', input: Number(pv_val), sd: Number(property_value_stdev), include: pv}, {datatype: 'median_age', input: Number(age_val), sd: Number(median_age_stdev), include: age}, {datatype: 'commute_time', input: Number(c_val), sd: Number(commute_time_stdev), include: c}];
-        
+        let datasetArr = [
+          {
+            datatype: "household_income",
+            input: Number(hi_val),
+            sd: Number(household_income_stdev),
+            include: hi
+          },
+          {
+            datatype: "property_value",
+            input: Number(pv_val),
+            sd: Number(property_value_stdev),
+            include: pv
+          },
+          {
+            datatype: "median_age",
+            input: Number(age_val),
+            sd: Number(median_age_stdev),
+            include: age
+          },
+          {
+            datatype: "commute_time",
+            input: Number(c_val),
+            sd: Number(commute_time_stdev),
+            include: c
+          }
+        ];
+
         let weightArr = [];
         let weight;
 
         function calcWeight() {
           for (let i = 0; i < datasetArr.length; i++) {
-            let {input, sd, datatype, include} = datasetArr[i];
+            let { input, sd, datatype, include } = datasetArr[i];
             let datapoint = Number(d[datatype]);
 
-            if(include) {
-        
-              if ((input - sd) < datapoint && datapoint < (input + sd)) {
+            if (include) {
+              if (input - sd < datapoint && datapoint < input + sd) {
                 weightArr.push(1);
-              } else if ((input - 2 * sd) < datapoint && datapoint < (input + 2 * sd)) {
+              } else if (
+                input - 2 * sd < datapoint &&
+                datapoint < input + 2 * sd
+              ) {
                 weightArr.push(2);
-              } else if ((input - 3 * sd) < datapoint && datapoint < (input + 3 * sd)) {
+              } else if (
+                input - 3 * sd < datapoint &&
+                datapoint < input + 3 * sd
+              ) {
                 weightArr.push(3);
-              }else if ((input - 4 * sd) < datapoint && datapoint < (input + 4 * sd)) {
+              } else if (
+                input - 4 * sd < datapoint &&
+                datapoint < input + 4 * sd
+              ) {
                 weightArr.push(4);
-              }else if ((input - 5 * sd) < datapoint && datapoint < (input + 5 * sd)) {
+              } else if (
+                input - 5 * sd < datapoint &&
+                datapoint < input + 5 * sd
+              ) {
                 weightArr.push(5);
-              }else if ((input - 6 * sd) < datapoint && datapoint < (input + 6 * sd)) {
+              } else if (
+                input - 6 * sd < datapoint &&
+                datapoint < input + 6 * sd
+              ) {
                 weightArr.push(6);
-              }else if ((input - 7 * sd) < datapoint && datapoint < (input + 7 * sd)) {
+              } else if (
+                input - 7 * sd < datapoint &&
+                datapoint < input + 7 * sd
+              ) {
                 weightArr.push(7);
-              }else if ((input - 8 * sd) < datapoint && datapoint < (input + 8 * sd)) {
+              } else if (
+                input - 8 * sd < datapoint &&
+                datapoint < input + 8 * sd
+              ) {
                 weightArr.push(8);
               } else {
-                weightArr.push(9); 
+                weightArr.push(9);
               }
             }
           }
-          return weight = Number(weightArr.reduce((total, current) => total + current, 0))/weightArr.length;
+          return (weight =
+            Number(weightArr.reduce((total, current) => total + current, 0)) /
+            weightArr.length);
         }
         calcWeight();
 
@@ -128,19 +194,19 @@ class Map extends Component {
           return color2;
         } else if (weight < 4) {
           return color3;
-        }else if (weight < 5) {
+        } else if (weight < 5) {
           return color4;
-        }else if (weight < 6) {
+        } else if (weight < 6) {
           return color5;
-        }else if (weight < 7) {
+        } else if (weight < 7) {
           return color6;
-        }else if (weight < 8) {
+        } else if (weight < 8) {
           return color7;
-        }else if (weight < 9) {
+        } else if (weight < 9) {
           return color8;
         } else {
-          return 'white'}
-
+          return "white";
+        }
       })
       .on("click", function(d) {
         mapContext.props.getActiveCounty(d.id);
@@ -179,13 +245,30 @@ class Map extends Component {
   render() {
     return (
       <div className="map">
-      <input onChange={(e) => this.setState({val: e.target.value})}/>
-      <select className='select-dataset' onChange={e => this.setState({dataset: e.target.value})}>
-        <option value={this.props.county.standardDeviation.household_income_stdev}>Household Income</option>
-        <option value={this.props.county.standardDeviation.property_value_stdev}>Property Value</option>
-        <option value={this.props.county.standardDeviation.commute_time_stdev}>Commute Time</option>
-        <option value={this.props.county.standardDeviation.median_age_stdev}>Median Age</option>
-      </select>
+        <input onChange={e => this.setState({ val: e.target.value })} />
+        <select
+          className="select-dataset"
+          onChange={e => this.setState({ dataset: e.target.value })}
+        >
+          <option
+            value={this.props.county.standardDeviation.household_income_stdev}
+          >
+            Household Income
+          </option>
+          <option
+            value={this.props.county.standardDeviation.property_value_stdev}
+          >
+            Property Value
+          </option>
+          <option
+            value={this.props.county.standardDeviation.commute_time_stdev}
+          >
+            Commute Time
+          </option>
+          <option value={this.props.county.standardDeviation.median_age_stdev}>
+            Median Age
+          </option>
+        </select>
         <div className="map-container" />
         <ActiveCounty />
       </div>
